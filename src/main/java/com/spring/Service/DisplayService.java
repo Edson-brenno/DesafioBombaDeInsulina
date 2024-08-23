4e7594e3d59c116ec10750b5bd050538cbd297d1
brenno <brennorebeiro@gmail.com>
2024-08-23 02:19:08 -0300

    feat: add DisplayService

---
+++ src/main/java/com/spring/Service/DisplayService.java
@@ -0,0 +1,15 @@
+package com.spring.Service;
+
+/**
+ * Display service
+ * Serviço que simula display*/
+public class DisplayService {
+
+    // Shows a message
+    // Mostra mensagem
+    public void display(String message) {
+
+        System.out.println(message);
+
+    }
+}
