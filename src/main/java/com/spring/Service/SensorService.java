5df1ace851dbb13d695483cc293d5e3e535814e1
brenno <brennorebeiro@gmail.com>
2024-08-23 01:04:40 -0300

    Add service sensor

---
+++ src/main/java/com/spring/Service/SensorService.java
@@ -0,0 +1,15 @@
+package com.spring.Service;
+
+import java.util.Random;
+
+/**
+ * This service it's responsible to simulate a work of a sensor
+ * Este servico procura simular o funcionamento de um sensor de glicose*/
+public class SensorService {
+
+    // This static method it'll generate random information of the sugar information
+    // Este metodo vai gerar nivéis diferentes do nivel de açucar
+    public static int obterInformacoesAcucarNoSangue(){
+        return new Random().nextInt(80,60);
+    }
+}
